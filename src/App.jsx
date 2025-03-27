6ea6d1c5310b434cb22f982c769d949692153d13
Nama Kamu <nizarrarfan@gmail.com>
2025-03-27 15:17:50 +0700

    fisrt commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import HomePage from "./components/HomePage";
+import HomePage from "./components/Pages/HomePage";
 
 const App = () => {
   return (
---
+++ src/components/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroContent from "../layouts/HeroContent";
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeroContent />
+      {/* <ListAnime/> */}
+    </div>
+  );
+};
+
+export default HomePage;
